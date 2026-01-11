660bdd7340d1cc42bb8f9cfa744fd60757b0fc09
gerardordiaz <jettyrats@gmail.com>
2026-01-11 11:48:36 -0600

    se agrega una sesion para no perder info, correcciones ortograficas y formato de codigo

--- app.py
+++ app.py
@@ -4,24 +4,34 @@ import plotly.express as px
 
 df = pd.read_csv("vehicles_us.csv")
 
-st.header("ABALISIS DE VEHICULOS")
-boton_histograma = st.button("construir histograma")
+st.header("ANALISIS DE VEHICULOS")
+# PRIMERO QUIERO HACER UNA SESION QUE NO SE INTERRUMPA
 
-if boton_histograma:
+if "show_hist" not in st.session_state:
+    st.session_state.show_hist = False
+
+if "show_scatter" not in st.session_state:
+    st.session_state.show_scatter = False
+
+# creacion de botones
+
+if st.button("construir histograma"):
+    st.session_state.show_hist = True
+
+if st.button("grafico de dispersion"):
+    st.session_state.show_scatter = True
+
+# histograma
+
+if st.session_state.show_hist:
+    st.write("Creación de un histograma para el conjunto de datos")
+    fig_hist = px.histogram(df, x="odometer")
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# grafico de dispersion
+
+if st.session_state.show_scatter:
     st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
-    # crea un histograma
-    fig = px.histogram(df, x="odometer")
-
-    # muestra el grafico
-    st.plotly_chart(fig, use_container_width=True)
-
-boton_dispersion = st.button("gráfico de dispersión")
-
-if boton_dispersion:
-    st.write(
-        "crea un grafico de dispersion para el conjunto de datos de anuncios de venta de coches"
-    )
-    fig = px.scatter(df, x="odometer", y="price")
-    st.plotly_chart(fig)
+        "Creación de gráfico de dispersion para el conjunto de datos de venta de coches")
+    fig_scatter = px.scatter(df, x="odometer", y="price")
+    st.plotly_chart(fig_scatter, use_container_width=True)
